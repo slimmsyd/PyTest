6edf2dc38da0265e0cc803f18860c4f1ced4651a
Sydney Sanders <ssanderss444@gmail.com>
2024-10-30 15:48:54 -0400

    added the health

--- app.py
+++ app.py
@@ -27,6 +27,10 @@ def hello():
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
+@app.route('/health')
+def health_check():
+    return "listening"
+
 
 if __name__ == '__main__':
    app.run()
